621196edff8ad755f13d25872bc71aa1d8fa57a2
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2020-10-09 12:58:49 +0330

    Students init

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,5 @@ Route::get('/', function () {
 Route::get('/dashboard', [App\Http\Controllers\DashboardController::class, 'index'])->name(
     'dashboard'
 );
+
+Route::resource('/student', App\Http\Controllers\StudentsController::class);
