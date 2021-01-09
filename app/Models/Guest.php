c6d2a7c7a8d75331bdaa60edbe525bb29c0822f6
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-01-09 07:04:54 +0330

    uuid trait and models

---
+++ app/Models/Guest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Guest extends Model
+{
+    use HasFactory, Notifiable, Uuids, SoftDeletes;
+}
---
+++ app/Models/Property.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Uuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Notifications\Notifiable;
+
+class Property extends Model
+{
+    use HasFactory, Notifiable, Uuids, SoftDeletes;
+}
