e349d298f378f1c9b56c4466ede7311cb8f21ea2
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-01-09 10:01:11 +0330

    just basic page for those who want to see

---
+++ resources/views/app.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;600&display=swap" rel="stylesheet">
+    <script src="https://www.google.com/recaptcha/api.js?onload=vueRecaptchaApiLoaded&render=explicit" async defer>
+    </script>
+
+    <title>{{env('APP_NAME')}}</title>
+</head>
+
+<body>
+    <div id="app">
+        Hey
+    </div>
+
+</body>
+
+</html>
