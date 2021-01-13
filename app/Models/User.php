0cff46801f11d0a311a973420c0898253fdebf42
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-01-13 07:29:41 +0330

    add passport

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,10 +8,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable, Uuids, SoftDeletes;
+    use HasApiTokens, HasFactory, Notifiable, Uuids, SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
