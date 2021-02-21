ae309b5a9c67ae3a192ddc7de80ec5020de4dcd1
Mohammad Javad Ghasemy <geeksesi@gmail.com>
2021-02-21 13:27:32 +0330

    header navbar

--- client/components/layouts/HeaderLayout.js
+++ client/components/layouts/HeaderLayout.js
@@ -9,10 +9,11 @@ const header = () => {
             <div className="logo" />
             <Menu theme="dark" mode="horizontal" defaultSelectedKeys={["2"]}>
                 <Menu.Item key="1">
-                    <Link href="/login">login</Link>
+                    <Link href="/login">ورود/ثبت‌نام</Link>
                 </Menu.Item>
-                <Menu.Item key="2">nav 2</Menu.Item>
-                <Menu.Item key="3">nav 3</Menu.Item>
+                <Menu.Item key="2">
+                    <Link href="/inventory">انبار داری</Link>
+                </Menu.Item>
             </Menu>
         </Header>
     );
